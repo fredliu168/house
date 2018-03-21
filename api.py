c3eaa99e897eeac209c43e57a5cdb8fa0d3730ca
fredliu168 <fred@qzcool.com>
2018-03-21 19:40:02 +0800

    新增条件搜索

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ from mysql_db.mysql import *
 import util
 import config
 from  model.room import *
-
+import  re
 
 class MyResponse(Response):
     @classmethod
@@ -26,7 +26,7 @@ app.response_class = MyResponse
 def source_img(image_name):
     # 获取资源文件
 
-    img_local_path = "{}/{}".format(config.g_source_img_dir,image_name)
+    img_local_path = "{}/{}".format(config.g_source_img_dir, image_name)
 
     img_stream = ''
 
@@ -100,6 +100,7 @@ def avatar(imageid):
     resp = Response(img_stream, mimetype="image/jpeg")
     return resp
 
+
 @app.route("/rent-room/<sha_identity>")
 def get_rent_room_detail(sha_identity):
     # 获取租赁信息
@@ -126,7 +127,7 @@ def get_rent_room_detail(sha_identity):
     room['start_time'] = obj_room.start_time.strftime('%Y-%m-%d')
     room['end_time'] = obj_room.end_time.strftime('%Y-%m-%d')
 
-    if obj_room.company_name!=None and len(obj_room.company_name) > 10:
+    if obj_room.company_name != None and len(obj_room.company_name) > 10:
         room['company_name'] = obj_room.company_name[:10] + '...'
 
     ret = dbManager.exec_sql(image_sql)
@@ -139,6 +140,7 @@ def get_rent_room_detail(sha_identity):
     result["msg"] = "获取数据成功"
 
     return result
+
 
 @app.route("/room/<sha_identity>")
 def get_room_detail(sha_identity):
@@ -166,8 +168,8 @@ r.phone = u.phone where sha_identity = '{sha_identity}'
     room['start_time'] = obj_room.start_time.strftime('%Y-%m-%d')
     room['end_time'] = obj_room.end_time.strftime('%Y-%m-%d')
 
-    if len(obj_room.company_name) >10:
-        room['company_name'] = obj_room.company_name[:10]+'...'
+    if len(obj_room.company_name) > 10:
+        room['company_name'] = obj_room.company_name[:10] + '...'
 
     ret = dbManager.exec_sql(image_sql)
 
@@ -180,21 +182,14 @@ r.phone = u.phone where sha_identity = '{sha_identity}'
 
     return result
 
-@app.route("/rent-search-house/<key_word>/<int:page>")
-def rent_search_house(key_word,page):
+
+def get_rent_house(sql,page):
+    print(sql)
     result = {"code": 10000, "value": "", "msg": ""}
 
     if page <= 0:
         result = {"code": -10000, "value": "", "msg": "page 必须大于0"}
         return result
-
-    sql = """select r.id,area,rent_type,sha_identity,title,price,r.phone,r.post_time,start_time,end_time,house_name,position,
-        floor,total_floor,has_kitchen_bath,lobby,live_room,orientation,
-        r.type,mark,name,u.type as utype,avatar,verify,company_name,company_addr  from rent_room r left join user u on
-        r.phone = u.phone where r.title like '%{key_word}%' or r.house_name like '%{key_word}%'  ORDER BY r.`post_time` DESC  LIMIT {page}, {offset}""".format(key_word = key_word,
-        page=(page - 1) * 10, offset=10)
-
-    print(sql)
 
     rooms = dbManager.exec_sql(sql)
 
@@ -225,14 +220,61 @@ def rent_search_house(key_word,page):
 
     return result
 
+def rent_house_orderby(orderby):
+    # 对租房信息进行排序 orderby = '1_1_1_1'
+    # 第一个条件 1-5
+    # {'id': 1, 'text': '价格从低到高'},
+    # {'id': 2, 'text': '价格从高到低'},
+    # {'id': 3, 'text': '面积从小到大'},
+    # {'id': 4, 'text': '面积从大到小'},
+    # {'id': 5, 'text': '发布时间'}
+    # 第二个条件 1-4
+    # {'id': 1, 'text': '1000元以下'},
+    # {'id': 2, 'text': '1000元-2000元'},
+    # {'id': 3, 'text': '2000元-3000元'},
+    # {'id': 4, 'text': '3000元以上'}
+    # 第三个条件 1-4
+    # {'id': 1, 'text': '一房'},
+    # {'id': 2, 'text': '两房'},
+    # {'id': 3, 'text': '三房'},
+    # {'id': 4, 'text': '四房'},
+    # 第四个条件 1-3
+    # {'id': 1, 'text': '住宅'},
+    # {'id': 2, 'text': '商铺'},
+    # {'id': 3, 'text': '写字楼'},
+    condition1 = {'0':'','1':'price','2':'price DESC','3':'area','4':'area DESC','5':'post_time'}
+    condition2 = {'0':'','1':'price < 1000','2':'price >= 1000 and price < 2000','3':'price >= 2000 and price < 3000','4':'price >= 3000 '}
+    condition3 = {'0':'','1':'live_room="一间"','2':'live_room="两间"','3':'live_room="三间"','4':'live_room="四间"'}
+    condition4 = {'0': '', '1': 'r.type="住宅"', '2': 'r.type="商铺"', '3': 'r.type="写字楼"'}
+
+    orderby = '1_1_1_1'
+
+    condition =
+
+
+
+@app.route("/rent-search-house/<key_word>/<int:page>")
+def rent_search_house(key_word, page):
+    #对字符进行过滤
+    r1 = u'[a-zA-Z0-9’!"#$%&\'()*+,-./:;<=>?@，。?★、…【】《》？“”‘’！[\\]^_`{|}~]+'  # 用户也可以在此进行自定义过滤字符
+    r2 = u'\s+;'
+
+    key_word_temp = re.sub(r1, '', key_word)
+
+
+    sql = """select r.id,area,rent_type,sha_identity,title,price,r.phone,r.post_time,start_time,end_time,house_name,position,
+        floor,total_floor,has_kitchen_bath,lobby,live_room,orientation,
+        r.type,mark,name,u.type as utype,avatar,verify,company_name,company_addr  from rent_room r left join user u on
+        r.phone = u.phone where r.title like '%{key_word}%' or r.house_name like '%{key_word}%'  ORDER BY r.`post_time` DESC  LIMIT {page}, {offset}""".format(
+        key_word=key_word_temp,
+        page=(page - 1) * 10, offset=10)
+
+    return get_rent_house(sql,page)
+
+
 @app.route("/rent-house/<int:page>")
 def rent_house(page=1):
     # 返回出租房屋
-    result = {"code": 10000, "value": "", "msg": ""}
-
-    if page <= 0:
-        result = {"code": -10000, "value": "", "msg": "page 必须大于0"}
-        return result
 
     sql = """select r.id,area,rent_type,sha_identity,title,price,r.phone,r.post_time,start_time,end_time,house_name,position,
     floor,total_floor,has_kitchen_bath,lobby,live_room,orientation,
@@ -240,34 +282,8 @@ def rent_house(page=1):
     r.phone = u.phone ORDER BY r.`post_time` DESC  LIMIT {page}, {offset}""".format(
         page=(page - 1) * 10, offset=10)
 
-    rooms = dbManager.exec_sql(sql)
+    return get_rent_house(sql,page)
 
-    for room in rooms:
-        # print(room)
-        obj_room = Room()
-        util.dict2obj(room, obj_room)
-        # print(obj_room.phone)
-        image_sql = "select name from image i where i.room_sha_identity ='{room_sha_identity}'".format(
-            room_sha_identity=obj_room.sha_identity)
-
-        # print(user_sql)
-        if len(obj_room.title) > 32:
-            room['title'] = obj_room.title[:32] + '...'
-
-        room['post_time'] = obj_room.post_time.strftime('%Y-%m-%d')
-        room['start_time'] = obj_room.start_time.strftime('%Y-%m-%d')
-        room['end_time'] = obj_room.end_time.strftime('%Y-%m-%d')
-
-        ret = dbManager.exec_sql(image_sql)
-        #
-        # print(ret)
-        #
-        room['image'] = ret
-
-    result["value"] = rooms
-    result["msg"] = "获取数据成功"
-
-    return result
 
 @app.route("/house/<int:page>")
 def house(page=1):
